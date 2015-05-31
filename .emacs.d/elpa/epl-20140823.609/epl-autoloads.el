6a09c5bb9a16ac9ad80863d63dd811fec88cea16
anouarattn <bakri.anouar@gmail.com>
2015-05-31 20:43:38 +0100

    dd

--- .emacs.d/elpa/epl-20140823.609/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("epl.el") (21528 44496 499882 800000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
